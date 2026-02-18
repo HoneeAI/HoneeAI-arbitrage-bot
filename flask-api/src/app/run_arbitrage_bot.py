437fafc89048c473a9d9911ee8a5a9c0022777fd
HoneeAI <official@honee.ai>
2026-02-18 15:03:20 +0200

    Major UI update

---
+++ flask-api/src/app/run_arbitrage_bot.py
@@ -0,0 +1,18 @@
+"""Activate arbitrage bot. In a seperate file to run as a subprocess.
+"""
+
+import sys
+from pathlib import Path
+
+# Add project root to sys.path
+project_root = Path(__file__).resolve().parents[2]
+sys.path.append(str(project_root / 'src'))
+
+from trading_bot.trading_bot import bot
+
+def run_arbitrage_bot():
+    """Runs the trading bot."""
+    bot()
+
+if __name__ == "__main__":
+    run_trading_bot()
